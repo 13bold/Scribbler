a76902d61836f8c917ddc2fd7a038b4829e7bab9
Matt Patenaude <MattPat@mattpat.net>
2009-11-05 13:46:43 -0500

    relatively finished with the LFWebService interface

--- LFWebService.h
+++ LFWebService.h
@@ -18,6 +18,8 @@
 	NSString *sharedSecret;
 	NSString *clientID;
 	
+	NSString *sessionKey;
+	
 	LFTrack *currentTrack;
 	NSMutableArray *requestQueue;
 }
@@ -30,9 +32,14 @@
 @property(copy) NSString *APIKey;
 @property(copy) NSString *sharedSecret;
 @property(copy) NSString *clientID;
-@property(retain) LFTrack *currentTrack;
+@property(copy) NSString *sessionKey;
+@property(retain,readonly) LFTrack *currentTrack;
+
+// Session methods
+- (void)establishNewSession;
 
 // Track methods
+- (void)startPlayingTrack:(LFTrack *)theTrack;
 - (void)scrobbleTrackIfNecessary:(LFTrack *)theTrack;
 - (void)loveTrack:(LFTrack *)theTrack;
 - (void)banTrack:(LFTrack *)theTrack;
@@ -42,6 +49,18 @@
 // Delegate interface
 @interface LFWebServiceDelegate
 
+// Session management
+- (void)sessionStartedWithKey:(NSString *)theKey;
 
+// Track responses
+- (void)nowPlayingSucceededForTrack:(LFTrack *)theTrack;
+- (void)scrobbleSucceededForTrack:(LFTrack *)theTrack;
+- (void)loveSucceededForTrack:(LFTrack *)theTrack;
+- (void)banSucceededForTrack:(LFTrack *)theTrack;
+
+- (void)nowPlayingFailedForTrack:(LFTrack *)theTrack error:(NSError *)theError willRetry:(BOOL)willRetry;
+- (void)scrobbleFailedForTrack:(LFTrack *)theTrack error:(NSError *)theError willRetry:(BOOL)willRetry;
+- (void)loveFailedForTrack:(LFTrack *)theTrack error:(NSError *)theError willRetry:(BOOL)willRetry;
+- (void)banFailedForTrack:(LFTrack *)theTrack error:(NSError *)theError willRetry:(BOOL)willRetry;
 
 @end
